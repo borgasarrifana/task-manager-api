8a85bc8fe1ceaef2d2590025059d7f5eb569bda8
Ricardo Ferreira <ricardogemeo@gmail.com>
2026-08-23 16:49:50 +0100

    Configure enums to serialize as strings in JSON

--- TaskManager.Api/Program.cs
+++ TaskManager.Api/Program.cs
@@ -29,7 +29,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 builder.Services.AddAuthorization();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddScoped<ITaskService, TaskService>();
